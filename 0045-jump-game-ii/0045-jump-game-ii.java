8f17f07a49fc940897e2ed75e239a651a31cdeb5
Anshika <166846935+Anshikais@users.noreply.github.com>
2026-04-25 23:09:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,11 +1,12 @@
 class Solution {
     public int jump(int[] nums) {
+        int n = nums.length-1;
         int ans=0;
-        int end=0;
+        int end=0;  
         int fast=0;
-        for(int i=0; i<nums.length-1; i++){
+        for(int i=0; i<n; i++){
             fast = Math.max(fast , i+nums[i]);
-            if(fast>=nums.length-1){
+            if(fast>=n){
                 ans++;
                 break;
             }
