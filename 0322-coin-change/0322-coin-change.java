a86326c4f5e275e33ce44754974443a4d8c553b0
Anshika <166846935+Anshikais@users.noreply.github.com>
2026-04-28 14:57:00 +0530

    Time: 36 ms (11.64%), Space: 47.6 MB (8.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,42 +2,21 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int dp[][]=new int[n][amount+1];
-        for(int i=0;i<dp.length;i++)
-        {
+        for(int i=0;i<dp.length;i++){
             Arrays.fill(dp[i],-1);
         }
         int ans=solve(coins,amount,n-1,dp);
-       
-        if(ans==Integer.MAX_VALUE)
-        {
-            return -1;
-        }
-        return ans;
-        
+        if(ans==Integer.MAX_VALUE) return -1;
+        return ans; 
     }
     public int solve(int[]coins, int amount,int n,int dp[][]){
-    // base case
-    if(amount==0)
-    {
-        return 0;
-    }
-       if(n<0)
-       {
-        return Integer.MAX_VALUE;
-         
-       }
-       
-       if(dp[n][amount]!=-1)
-       {
-        return dp[n][amount];
-       }
+    if(amount==0) return 0;
+     if(n<0) return Integer.MAX_VALUE;
+       if(dp[n][amount]!=-1)return dp[n][amount];
         int include = Integer.MAX_VALUE;
-
-
       if(coins[n]<=amount){
          include=solve(coins, amount-coins[n],n,dp);
-         if(include!=Integer.MAX_VALUE)
-         {
+         if(include!=Integer.MAX_VALUE){
             include+=1;
          }
       }
