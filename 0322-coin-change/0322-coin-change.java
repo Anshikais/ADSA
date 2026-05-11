a401720b436ff3e5451ead93f25821c98de77f53
Anshika <166846935+Anshikais@users.noreply.github.com>
2026-05-11 12:04:36 +0530

    Time: 15 ms (81.8%), Space: 46.3 MB (70.74%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,26 +1,18 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int dp[][]=new int[n][amount+1];
-        for(int i=0;i<dp.length;i++){
-            Arrays.fill(dp[i],-1);
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++){
+            for(int j=0; j<coins.length; j++){
+                if(i-coins[j]>=0){
+              dp[i] = Math.min(dp[i],1+dp[i-coins[j]]);
+                }
+                
+            }
         }
-        int ans=solve(coins,amount,n-1,dp);
-        if(ans==Integer.MAX_VALUE) return -1;
-        return ans; 
-    }
-    public int solve(int[]coins, int amount,int n,int dp[][]){
-    if(amount==0) return 0;
-      if(n<0) return Integer.MAX_VALUE;
-       if(dp[n][amount]!=-1)return dp[n][amount];
-      int include = Integer.MAX_VALUE;
-       if(coins[n]<=amount){
-        include = solve(coins, amount-coins[n],n,dp);
-        if(include!=Integer.MAX_VALUE){
-            include++;
-        }
-       }
-         int exclude = solve(coins,amount,n-1,dp);
-         return dp[n][amount]= Math.min(include,exclude); 
+        if(dp[amount]!=amount+1) return dp[amount];
+            return -1;
+        
     }
 }
