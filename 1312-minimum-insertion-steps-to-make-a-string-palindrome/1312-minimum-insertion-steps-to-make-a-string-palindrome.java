564c65a92de16e52b09525ce0b63eb19977dada8
Anshika <166846935+Anshikais@users.noreply.github.com>
2026-05-22 10:17:32 +0530

    Time: 35 ms (16.51%), Space: 46.6 MB (91.63%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for(int i=0; i<n; i++){
+            Arrays.fill(dp[i],-1);
+        }
+         int lcs =solve(s,0,n-1,dp);
+        return n-lcs;
+    }
+    public int solve(String s, int i, int j, int[][]dp){
+       if(i > j) return 0;
+        if(i == j) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int take = 0;
+        if(s.charAt(i)==s.charAt(j)){
+            take = 2+solve(s,i+1,j-1,dp);
+        }
+        int skip = solve(s,i+1,j,dp);
+        int skip1 = solve(s,i,j-1,dp);
+       return dp[i][j] = Math.max(take,Math.max(skip,skip1));
+        
+    }
+}
