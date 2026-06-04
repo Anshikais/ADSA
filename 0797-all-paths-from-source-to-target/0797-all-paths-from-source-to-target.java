4b9645486311eac0d3a3e878d01bf77cc2c05f49
Anshika <166846935+Anshikais@users.noreply.github.com>
2026-06-04 16:22:32 +0530

    Time: 2 ms (91.9%), Space: 48 MB (41.46%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        dfs(0, graph, path, ans);
+        return ans;
+    } 
+    public void dfs(int node, int[][]graph, List<Integer>path, List<List<Integer>>ans){
+        path.add(node);
+        if(node==graph.length-1){
+            ans.add(new ArrayList<>(path));
+        } else {
+            for (int next : graph[node]) {
+                dfs(next, graph, path, ans);
+            }
+       }
+       path.remove(path.size()-1);
+    }
+}
