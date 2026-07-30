92c7a414f732d4f65191606f21534fb03ca64d14
Anshika <166846935+Anshikais@users.noreply.github.com>
2026-07-30 23:37:40 +0530

    Time: 0 ms (100%), Space: 43.2 MB (47.13%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -3,7 +3,7 @@ class Solution {
         int n = word.length();
          int ans = 0;
          for(int i=0; i<n; i++){
-            ans +=(i/8) + 1;
+            ans +=(i/8) + 1;// jabtak value i ki less than hai 8 ke tabtak 1 add hoga uske baad jo value aayega wo 
          }
          return ans;
     }
